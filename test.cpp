35db54d4dc40cfc4f7a9d152a8eaf14bcb94d4b5
PramodJose <pramod.gj17@gmail.com>
2018-10-10 17:00:04 +0530

    End of day commit

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
 class Vertex
@@ -8,13 +10,13 @@ public:
 	unsigned int indegree, adjacentVertices;
 	Vertex **adjVertVec;
 
-	Vertex()
+	/*Vertex()
 	{
 		indegree = 0;
 		vName = 0;
 		adjacentVertices = 0;
 		adjVertVec = NULL;
-	}
+	}*/
 
 	Vertex(int num)
 	{
@@ -25,15 +27,43 @@ public:
 	}
 };
 
+class Edge
+{
+private:
+	static short int count;
+public:
+	short int eName;
+	int weight;
+	Vertex *from, *to;
+
+	Edge()
+	{
+		eName = count++;
+		from = to = NULL;
+		weight = 0;
+	}
+
+	~Edge()
+	{
+		--count;
+	}
+};
+
+short int Edge::count = 1;
+
 class Graph
 {
 public:
 	Vertex **v;
+	vector<Edge> e;
 	int vertices;
+	int edges;
 
 	Graph(int size)
 	{
 		vertices = size;
+		edges = 0;
+
 		v = new Vertex*[vertices];
 		for(int i = 0; i < vertices; ++i)	// Create all the vertices.
 			v[i] = new Vertex(i + 1);
@@ -41,18 +71,26 @@ public:
 		for(int i = 0; i < vertices; ++i)
 		{
 			int n;
-			cout << "Enter number of vertices adjacent to Vertex " << i + 1<< " : ";
+			cout << "Enter number of vertices adjacent to Vertex " << (char)('A' + i)<< " : ";
 			cin >> v[i]->adjacentVertices;
 			n = v[i]->adjacentVertices;
+			edges += n;
 
 			v[i]->adjVertVec = new Vertex*[n];			// Create an array of Vertex pointers of size n, i.e. the number of adjacent vertices.
 			for(int j = 0; j < n; ++j)
 			{
-				int adjVertex;
+				int adjVertex, weight;
+				Edge newEdge;
 				cout << "Enter vertex number: ";
 				cin >> adjVertex;
 				v[i]->adjVertVec[j] = v[adjVertex -1];
 				v[adjVertex - 1]->indegree++;
+				cout << "Enter the edge weight: ";
+				cin >> weight;
+				newEdge.weight = weight;
+				newEdge.from = v[i];
+				newEdge.to = v[adjVertex -1];
+				e.push_back(newEdge);
 			}
 
 		}
@@ -60,11 +98,34 @@ public:
 
 	Vertex* operator[](int pos)
 	{
+		if (pos < 0 || pos >= vertices)
+			return NULL;
 		return v[pos];
 	}
+
+	friend ostream & operator << (ostream &out, const Graph &g); 
+    //friend istream & operator >> (istream &in,  Graph &g); 
 };
+
+ostream& operator <<(ostream &out, const Graph &g)
+{
+	out << "The graph is:-\n";
+
+	for(int i = 0; i < g.vertices; ++i)
+	{
+		out << g.v[i]->vName << " --> ";
+
+		for(int j = 0; j < g.v[i]-> adjacentVertices; ++i)
+			out << g.v[i]->adjVertVec[j]->vName << " -> ";
+
+		out << "NULL\n";
+	}
+
+	return out;
+}
 
 int main()
 {
-	Graph g(7);
+	Graph g(3);
+	cout << g;
 }
